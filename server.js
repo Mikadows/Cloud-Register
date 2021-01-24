56deb9825b5d3532cdab2ce8b55fad303db19c63
Gwendal <gwendou95@hotmail.fr>
2021-01-24 19:31:44 +0100

    :sparkles: Add user on click

--- server.js
+++ server.js
@@ -3,32 +3,32 @@ const middlewares = require('./middlewares/middlewares');
 const userDao = require('./dao/userDao');
 const express = require("express"), app = express();
 
-let user = {
-    fullName: "Jean Valjean2",
-    email: "Jean@Valjean.com",
-    phone: "+33655555555",
-    password: "Toto",
-}
-
-userDao.createUser(user);
-
 app.use(express.static(__dirname + "/public"));
 
 //start the app
 var port = process.env.PORT || process.env.LOCAL_PORT;
 app.listen(port, () => console.log('listening on port', port));
 
-app.post('/create_user', middlewares.parseData, function(req, res){
-    var fullname = req.body.fullname
-    var email = req.body.email
-    var phone = req.body.phone
-    var password = req.body.password
-    var repeatPassword = req.body.repeatPassword
+app.post('/create_user', middlewares.parseData, async function(req, res){
+    const fullName = req.body.fullname;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const password = req.body.password;
+    const repeatPassword = req.body.repeatPassword;
+
+    let user = {
+        fullName: fullName,
+        email: email,
+        phone: phone,
+        password: password,
+    }
 
     //todo create user model
-    console.log(fullname)
+    console.log(fullName)
     console.log(email)
     console.log(phone)
     console.log(password)
     console.log(repeatPassword)
+
+    await userDao.createUser(user);
 })
