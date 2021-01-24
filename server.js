8bd34cc34162fa0978aad8db19fefcf97b5c17df
Gwendal <gwendou95@hotmail.fr>
2021-01-24 18:31:29 +0100

    :sparkles: User insertion

--- server.js
+++ server.js
@@ -1,36 +1,19 @@
 require('dotenv').config();
-var express = require("express"), app = express()
-const Cloudant = require('@cloudant/cloudant');
-const vcap = require('./config/vcap-local.json');
+const userDao = require('./dao/userDao');
+const express = require("express"), app = express();
 
-dbCloudantConnect();
+
+let user = {
+    fullName: "Jean Valjean2",
+    email: "Jean@Valjean.com",
+    phone: "+33655555555",
+    password: "Toto",
+}
+
+userDao.createUser(user);
 
 app.use(express.static(__dirname + "/public"));
 
 //start the app
 var port = process.env.PORT || process.env.LOCAL_PORT;
 app.listen(port, () => console.log('listening on port', port));
-
-
-
-/**
- * Connects to the Cloudant DB
- * @return {Promise} - when resolved, contains the db, ready to go
- */
-function dbCloudantConnect() {
-    return new Promise((resolve, reject) => {
-        Cloudant({  // eslint-disable-line
-            url: vcap.services.cloudantNoSQLDB.credentials.url
-        }, ((err, cloudant) => {
-            if (err) {
-                console.log('Connect failure: ' + err.message + ' for Cloudant DB: ' +
-                    process.env.CLOUDANT_DB_NAME);
-                reject(err);
-            } else {
-                let db = cloudant.use(process.env.CLOUDANT_DB_NAME);
-                console.log('Connect success! Connected to DB: ' + process.env.CLOUDANT_DB_NAME);
-                resolve(db);
-            }
-        }));
-    });
-}
